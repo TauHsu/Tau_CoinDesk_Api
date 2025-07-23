24b7a6d7ebee2647a759a4a35440ddf4ad684e5e
Tau Hsu <jason850629@gmail.com>
2025-07-23 13:47:06 +0800

    Create Currency.cs
    Currencies 資料表結構

---
+++ src/Models/Currency.cs
@@ -0,0 +1,9 @@
+namespace Tau_CoinDesk_Api.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string ChineseName { get; set; } = string.Empty;
+    }
+}
