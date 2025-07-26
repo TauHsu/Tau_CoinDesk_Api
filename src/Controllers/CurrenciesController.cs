2a987b23e1506c54dfff1d1414d92a3b95828a2c
Tau Hsu <jason850629@gmail.com>
2025-07-27 01:56:39 +0800

    Update CurrenciesController.cs
    檔案路徑調整、更新為新的 response 格式

--- src/Controllers/CurrenciesController.cs
+++ src/Controllers/CurrenciesController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-using Tau_CoinDesk_Api.Models;
-using Tau_CoinDesk_Api.Services;
+using Microsoft.Extensions.Localization;
+using Tau_CoinDesk_Api.Models.Entities;
+using Tau_CoinDesk_Api.Models.Dto;
+using Tau_CoinDesk_Api.Interfaces.Services;
 
 namespace Tau_CoinDesk_Api.Controllers
 {
@@ -9,38 +11,52 @@ namespace Tau_CoinDesk_Api.Controllers
     public class CurrenciesController : ControllerBase
     {
         private readonly ICurrencyService _service;
+        private readonly IStringLocalizer<SharedResource> _localizer;
 
-        public CurrenciesController(ICurrencyService service)
+        public CurrenciesController(ICurrencyService service, IStringLocalizer<SharedResource> localizer)
         {
             _service = service;
+            _localizer = localizer;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetCurrencies()
         {
             var result = await _service.GetCurrenciesAsync();
-            return Ok(result);
+            var response = new ApiResponse<IEnumerable<object>>(true, _localizer["GetSuccess"], result);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCurrency(Guid id)
+        {
+            var result = await _service.GetOneCurrencyAsync(id);
+            var response = new ApiResponse<CurrencyDto>(true, _localizer["GetSuccess"], result);
+            return Ok(response);
         }
 
         [HttpPost]
         public async Task<IActionResult> PostCurrency(Currency currency)
         {
             var created = await _service.CreateCurrencyAsync(currency);
-            return CreatedAtAction(nameof(GetCurrencies), new { id = created.Id }, created);
+            var response = new ApiResponse<Currency>(true, _localizer["CreateSuccess"], created);
+            return CreatedAtAction(nameof(GetCurrencies), new { id = created.Id }, response);
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateCurrency(Guid id, Currency currency)
         {
-            await _service.UpdateCurrencyAsync(id, currency);
-            return NoContent();
+            var updated = await _service.UpdateCurrencyAsync(id, currency);
+            var response = new ApiResponse<bool>(true, _localizer["UpdateSuccess"], updated);
+            return Accepted(response);
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCurrency(Guid id)
         {
             await _service.DeleteCurrencyAsync(id);
-            return NoContent();
+            var response = new ApiResponse<object>(true, _localizer["DeleteSuccess"]);
+            return Ok(response);
         }
     }
 }
