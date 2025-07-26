e8c7678acdb391a7a5c5262c753cc5f92f3e50ea
Tau Hsu <jason850629@gmail.com>
2025-07-27 02:22:36 +0800

    Update Program.cs
    導入 .env 環境變數，因應新增功能調整。

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,50 @@
-using Tau_CoinDesk_Api.Data;
-using Tau_CoinDesk_Api.Services;
-using Tau_CoinDesk_Api.Repositories;
-using Tau_CoinDesk_Api.Middlewares;
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Localization;
+using Tau_CoinDesk_Api.Data;
+using Tau_CoinDesk_Api.Repositories;
+using Tau_CoinDesk_Api.Services;
+using Tau_CoinDesk_Api.Services.Encryption;
+using Tau_CoinDesk_Api.Services.Security;
+using Tau_CoinDesk_Api.Interfaces.Repositories;
+using Tau_CoinDesk_Api.Interfaces.Services;
+using Tau_CoinDesk_Api.Interfaces.Encryption;
+using Tau_CoinDesk_Api.Interfaces.Security;
+using Tau_CoinDesk_Api.Middlewares;
+
 using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 註冊 DbContext
+// 讀取 .env（僅本地開發用）
+var envPath = Path.Combine(Directory.GetCurrentDirectory(), "..", ".env");
+if (File.Exists(envPath))
+{
+    Env.Load(envPath);
+}
+
+builder.Configuration.AddEnvironmentVariables();
+
+// 設定資料庫
+var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
+
+// 其他設定
+var aesKey = builder.Configuration["AES:Key"];
+var aesIv = builder.Configuration["AES:IV"];
+var rsaPrivate = builder.Configuration["RSA:PrivateKeyPath"];
+var rsaPublic = builder.Configuration["RSA:PublicKeyPath"];
 
 builder.Services.AddHttpClient<ICoinDeskService, CoinDeskService>();
 builder.Services.AddScoped<IRatesService, RatesService>();
 builder.Services.AddScoped<ICurrencyRepository, CurrencyRepository>();
 builder.Services.AddScoped<ICurrencyService, CurrencyService>();
+builder.Services.AddScoped<ISecureCurrencyService, SecureCurrencyService>();
+
+// AES, RSA
+builder.Services.AddSingleton<IAesEncryptionStrategy, AesEncryptionStrategy>();
+builder.Services.AddSingleton<IRsaCertificateStrategy, RsaCertificateService>();
 
 // 多語系服務
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
@@ -40,7 +69,7 @@ var supportedCultures = new[]
 
 app.UseRequestLocalization(new RequestLocalizationOptions
 {
-    DefaultRequestCulture = new RequestCulture("en"),
+    DefaultRequestCulture = new RequestCulture("en"), // 預設語系為英文
     SupportedCultures = supportedCultures,
     SupportedUICultures = supportedCultures
 });
